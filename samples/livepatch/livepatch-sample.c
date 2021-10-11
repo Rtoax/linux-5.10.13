502096e34ea5ec5e94b7ec0d7250b0e4db6a4d45
rtoax <2386499836@qq.com>
2021-10-11 14:47:08 +0800

    livepatch demo

--- samples/livepatch/livepatch-sample.c
+++ samples/livepatch/livepatch-sample.c
@@ -11,6 +11,12 @@
 #include <linux/kernel.h>
 #include <linux/livepatch.h>
 
+#include <linux/sched.h>
+#include <linux/sched/loadavg.h>
+#include <linux/sched/stat.h>
+#include <linux/pid_namespace.h>
+#include <linux/proc_fs.h>
+
 /*
  * This (dumb) live patch overrides the function that prints the
  * kernel boot cmdline when /proc/cmdline is read.
@@ -36,6 +42,34 @@ static int livepatch_cmdline_proc_show(struct seq_file *m, void *v)
 	return 0;
 }
 
+
+#define LOAD_INT(x) ((x) >> FSHIFT)
+#define LOAD_FRAC(x) LOAD_INT(((x) & (FIXED_1-1)) * 100)
+
+static int livepatch_loadavg_proc_show(struct seq_file *m, void *v) 
+{
+	unsigned long avnrun[3];
+
+#if 0
+    get_avenrun(avnrun, FIXED_1/200, 0); 
+
+	seq_printf(m, "RongTao: %lu.%02lu %lu.%02lu %lu.%02lu %ld/%d %d\n",
+        LOAD_INT(avnrun[0]), LOAD_FRAC(avnrun[0]),
+        LOAD_INT(avnrun[1]), LOAD_FRAC(avnrun[1]),
+        LOAD_INT(avnrun[2]), LOAD_FRAC(avnrun[2]),
+        nr_running(), nr_threads,
+        idr_get_cursor(&task_active_pid_ns(current)->idr) - 1); 
+#elif 0
+	seq_printf(m, "RongTao: %lu.%02lu %lu.%02lu %lu.%02lu\n",
+		   LOAD_INT(avnrun[0]), LOAD_FRAC(avnrun[0]),
+		   LOAD_INT(avnrun[1]), LOAD_FRAC(avnrun[1]),
+		   LOAD_INT(avnrun[2]), LOAD_FRAC(avnrun[2]));
+#else
+	seq_printf(m, "RongTao: 11111\n");
+#endif
+	    return 0;
+}
+
 /**
  *  补丁函数
  */
@@ -43,6 +77,10 @@ static struct klp_func funcs[] = {
 	{
 		.old_name = "cmdline_proc_show",
 		.new_func = livepatch_cmdline_proc_show,
+	},
+	{
+		.old_name = "loadavg_proc_show",
+		.new_func = livepatch_loadavg_proc_show,
 	}, { }
 };
 
