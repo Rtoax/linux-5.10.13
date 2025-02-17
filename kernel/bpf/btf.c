35806ed758b607f417e4acb0a6a9cd5ece386ab5
Rong Tao <rongtao@cestc.cn>
2025-02-17 16:45:31 +0800

    rongtao
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -4423,9 +4423,9 @@ enum {
 	__ctx_convert_unused, /* to avoid empty enum in extreme .config */
 };
 static u8 bpf_ctx_convert_map[] = {
-//#define BPF_PROG_TYPE(_id, _name, prog_ctx_type, kern_ctx_type) \
-//	[_id] = __ctx_convert##_id,
-//#include <linux/bpf_types.h>
+#define BPF_PROG_TYPE(_id, _name, prog_ctx_type, kern_ctx_type) \
+	[_id] = __ctx_convert##_id,
+#include <linux/bpf_types.h>
 
 /**
  *  展开 #include <linux/bpf_types.h>
