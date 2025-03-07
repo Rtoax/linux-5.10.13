b8649bf4200c5dd1b0b2ea2fc407f0bdb02f81cb
Rong Tao <rongtao@cestc.cn>
2025-03-07 10:34:57 +0800

    lib/hweight.c: Update
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- lib/hweight.c
+++ lib/hweight.c
@@ -52,12 +52,12 @@ unsigned long __sw_hweight64(__u64 w)
 	w =  (w + (w >> 4)) & 0x0f0f0f0f0f0f0f0ful;
 	return (w * 0x0101010101010101ul) >> 56;
 #else
-//	__u64 res = w - ((w >> 1) & 0x5555555555555555ul);
-//	res = (res & 0x3333333333333333ul) + ((res >> 2) & 0x3333333333333333ul);
-//	res = (res + (res >> 4)) & 0x0F0F0F0F0F0F0F0Ful;
-//	res = res + (res >> 8);
-//	res = res + (res >> 16);
-//	return (res + (res >> 32)) & 0x00000000000000FFul;
+	__u64 res = w - ((w >> 1) & 0x5555555555555555ul);
+	res = (res & 0x3333333333333333ul) + ((res >> 2) & 0x3333333333333333ul);
+	res = (res + (res >> 4)) & 0x0F0F0F0F0F0F0F0Ful;
+	res = res + (res >> 8);
+	res = res + (res >> 16);
+	return (res + (res >> 32)) & 0x00000000000000FFul;
 #endif
 }
 EXPORT_SYMBOL(__sw_hweight64);
