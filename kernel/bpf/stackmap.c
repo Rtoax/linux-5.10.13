0e3c727754149a629221de1e8b9aacf35efe7acb
Rong Tao <rongtao@cestc.cn>
2025-08-03 18:59:17 +0800

    bpf_get_stackid
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- kernel/bpf/stackmap.c
+++ kernel/bpf/stackmap.c
@@ -477,8 +477,8 @@ long bpf_get_stackid(void *ctx, struct bpf_map *map, u64 flags){}//+++
 BPF_CALL_3(bpf_get_stackid, struct pt_regs *, regs, struct bpf_map *, map,
 	   u64, flags)
 {
-    struct bpf_map *map;//+++
-    u64 flags;//+++
+	struct bpf_map *map;//+++
+	u64 flags;//+++
 
 	u32 max_depth = map->value_size / stack_map_data_size(map);
 	/* stack_map_alloc() checks that max_depth <= sysctl_perf_event_max_stack */
